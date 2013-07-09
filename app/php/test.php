2e531ff9352927a65121a3c2f7ff951720db19e9
levick <levente.opelcz@gmail.com>
2013-07-08 20:25:35 -0700

    Loading images for projects and just make things dynamic.

---
+++ app/php/test.php
@@ -0,0 +1,23 @@
+<?php
+	// get all images in a directory and return them in json
+	
+	if (isset($_GET['imgdir']) && $_GET['imgdir'] != 'undefined') {
+		$directory = '../img/' . $_GET['imgdir'] . '/';
+		
+		if ($dirhandler = opendir($directory)) {
+			$numfiles = 0;
+			
+			while ($file = readdir($dirhandler)) {
+				if ($file != '.' && $file != '..') {
+					$files[$numfiles] = $file;
+					$numfiles++;
+				}
+			}
+			
+			closedir($dirhandler);
+			
+			// convert it to json and make json secure with angular thus the ")]}',\n" prefix
+			echo ")]}',\n" . json_encode($files);
+		}
+	}
+?>

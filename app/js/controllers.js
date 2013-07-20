27a6a0f4fb2530fb8f235314f68186335fb87a6c
levick <levente.opelcz@gmail.com>
2013-07-19 21:29:14 -0700

    Hover polish, standardization and layout fizes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -35,20 +35,22 @@ angular.module('myApp.controllers', [])
 				what:		['UX/UI', 'Time/Team/Client Management', 'Coding(JavaScript(jQuery))'],
 				client:	'CBC - Marketplace',
 				info: 	"Suddenly we got an opportunity to work for CBC Marketplace on an online, interactive html5 and css3 based (so no flash) sodium calculator for their upcoming show. The deadline was super close, we only got two weeks before the show started. Becuse of budget and timing, Ayogo assigned this project to me as a one man army, though I got a programmer intern to help me out and start programming the logic parallel while I was designing the UI and UX. In this two week, my responsibilities spanned from talking to the client, keeping my boss in the loop, building a fast and non-redundant work process between me and Allen (my little helper, who helped a LOT), Designing UX/UI from scratch, using stock icons where possible for foods and at the end, ironing out bugs in the code. Oh, almost forgot to mention, that this 2 week was shortened by 2 days also because I requested some vacation time off before we even knew about this project, and that vacation time was really precious to me, because my girlfriend left the country, so wanted to spend my last days with her only. So even with -2 days, we delivered before time, way under budget and the client was super happy and impressed with the quality of work in this short time we had to deliver.",
-				imgdir:	'cbc-sodium'
+				imgdir:	'zootaire'
 			},
 			{
 				title: 	'I<3J', 
 				at: 		'Ayogo Inc.',
 				what:		['UX', 'Coding(C#, Java)'],
 				client: 'Avery Dennison - Vancive',
-				info:		"A project that I feel... failed. So why am I showing this? I think we can learn just as much from failures (if not more) than successes. Also this project involved a lot of heat, pressure, frustration and cutting edge technology and was overall fun and really interesting. Also was my first mobile unity project. And if that's not enough, throw in a cutting edge hardware and software (it came in parts so we had to assemble it ourselves), a non-exsiting API and Bluetooth. All served cold. :D hahaha. So Avery Dennison hired Ayogo to create an Android mobile game that uses their prototype patch to measure all kinds of activity data like heart rate, respiration rate, body temperature, sweat level, movement (gyro, or steps) and so on. This little patch saves data internally. In certain periods it tryes to connect to a mobile device and sends it's saved data throught Bluetooth for further processing. The client wanted a game that reacts to this 'real time' data feed in real time and you know, just be fun, casual and make kids (oh right, we targeted kids) get up and do a little exercise now and then. The game is meant to be showcased with the patch, and to see what and how the patch can be used and integrated. We did real life tests of just sitting and being calm, walking, running, doing jumpingjacks, doing a full workout and such to realise that the patch was unreliable in measuring bio data and in connectivity. The showcases still wen't smoothly though, but just wasn't using live data from the patches. Also we wen't way over budget (around 300%) and time too."
+				info:		"A project that I feel... failed. So why am I showing this? I think we can learn just as much from failures (if not more) than successes. Also this project involved a lot of heat, pressure, frustration and cutting edge technology and was overall fun and really interesting. Also was my first mobile unity project. And if that's not enough, throw in a cutting edge hardware and software (it came in parts so we had to assemble it ourselves), a non-exsiting API and Bluetooth. All served cold. :D hahaha. So Avery Dennison hired Ayogo to create an Android mobile game that uses their prototype patch to measure all kinds of activity data like heart rate, respiration rate, body temperature, sweat level, movement (gyro, or steps) and so on. This little patch saves data internally. In certain periods it tryes to connect to a mobile device and sends it's saved data throught Bluetooth for further processing. The client wanted a game that reacts to this 'real time' data feed in real time and you know, just be fun, casual and make kids (oh right, we targeted kids) get up and do a little exercise now and then. The game is meant to be showcased with the patch, and to see what and how the patch can be used and integrated. We did real life tests of just sitting and being calm, walking, running, doing jumpingjacks, doing a full workout and such to realise that the patch was unreliable in measuring bio data and in connectivity. The showcases still wen't smoothly though, but just wasn't using live data from the patches. Also we wen't way over budget (around 300%) and time too.",
+				imgdir:	'zootaire'
 			},
 			{
 				title:	'Ayogo Website Update', 
 				at:			'Ayogo Inc.',
 				what:		['UI', 'Coding(HTML(5), CSS(3), JavaScript(jQuery, AngularJS))'],
-				info:		'Something here...'
+				info:		'Something here...',
+				imgdir:	'zootaire'
 			}
 		];
 		

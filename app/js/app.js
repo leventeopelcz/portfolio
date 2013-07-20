87dd208d62ada9a2ed74d488605ba583dd09d025
levick <levente.opelcz@gmail.com>
2013-07-20 01:41:45 -0700

    Refresh bug mostly fixed, buggy image refresh coz ajax.

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		
 		$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'home_ctrl'});
     $routeProvider.when('/project/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});

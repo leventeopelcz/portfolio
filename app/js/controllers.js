cb378357a7adfe0d89fae402edbc46ea9e2507ea
levick <levente.opelcz@gmail.com>
2013-10-19 03:00:34 -0700

    Restructure, modularize, finally start to look nice!
    And works like a charm too, no bug found.
    Some optimization still needed:
    - Put back skrollr...
    - naming
    - better services API (load only parts of json and images)
    - don't send request if it's been requested and loaded (I dunno this one yet :/)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,22 +8,28 @@ angular.module('myApp.controllers', [])
 //----- HOME CONTROLLER -----//
 //---------------------------------------------
 
-.controller('home_ctrl', ['Projects', function(Projects) {
+.controller('home_ctrl', ['Projects', '$scope', function(Projects, $scope) {
 	$("[data-toggle='tooltip']").tooltip();
+	Projects.get(function(response) {
+		$scope.projects = response;
+	});
 }])
 
 //---------------------------------------------
-//----- PROJECTS CONTROLLER -----//
+//----- HOME THUMBS CONTROLLER -----//
 //---------------------------------------------
 
-.controller('projects_ctrl', ['Projects', 'Images', '$timeout', '$rootScope', '$scope', function(Projects, Images, $timeout, $rootScope, $scope) {
-	Projects.getProjects(function() {
-		var image = 'img/' + $rootScope.projects[$scope.$index].imgdir + '/' + $rootScope.projects[$scope.$index].imgs[0];
+.controller('home_thumbs_ctrl', ['ProjectImages', 'Images', '$scope', function(ProjectImages, Images, $scope) {
+	ProjectImages.get($scope.projects[$scope.$index].imgdir, function(response) {
+		$scope.projectImages = response;
 		$scope.imageLoading = true;
+		var image = 'img/' + $scope.projects[$scope.$index].imgdir + '/' + $scope.projectImages[0];
 		Images.preload(image, function() {
 			$scope.stopSpinner();
 			$scope.imageLoading = false;
-			$scope.$apply();
+			if(!$scope.$$phase) {
+				$scope.$digest();
+			}
 		});
 	});
 }])
@@ -32,87 +38,43 @@ angular.module('myApp.controllers', [])
 //----- PROJECT CONTROLLER -----//
 //---------------------------------------------
 
-.controller('project_ctrl', ['$scope', '$routeParams', 'Projects', '$rootScope', '$timeout', function($scope, $routeParams, Projects, $rootScope, $timeout) {
-	Projects.getProjects(function() {
-		$scope.title 	= $rootScope.projects[$routeParams.projectId - 1].title;
-		$scope.at 		= $rootScope.projects[$routeParams.projectId - 1].at;
-		$scope.what 	= $rootScope.projects[$routeParams.projectId - 1].what;
-		$scope.client = $rootScope.projects[$routeParams.projectId - 1].client;
-		$scope.info 	= $rootScope.projects[$routeParams.projectId - 1].info;
-		$scope.imgdir	= $rootScope.projects[$routeParams.projectId - 1].imgdir;
-		$scope.imgs		= $rootScope.projects[$routeParams.projectId - 1].imgs;
-		
-		$scope.dropdowns 	= $rootScope.projects[$routeParams.projectId - 1].dropdowns;
-		$scope.links 			= $rootScope.projects[$routeParams.projectId - 1].links;
-		
+.controller('project_ctrl', ['Projects', 'ProjectImages', '$scope', '$routeParams', function(Projects, ProjectImages, $scope, $routeParams) {
+	$("[data-toggle='tooltip']").tooltip();
+	Projects.get(function(response) {
+		$scope.projects = response;
+		$scope.title 			= $scope.projects[$routeParams.projectId - 1].title;
+		$scope.at 				= $scope.projects[$routeParams.projectId - 1].at;
+		$scope.what 			= $scope.projects[$routeParams.projectId - 1].what;
+		$scope.client 		= $scope.projects[$routeParams.projectId - 1].client;
+		$scope.info 			= $scope.projects[$routeParams.projectId - 1].info;
+		$scope.imgdir			= $scope.projects[$routeParams.projectId - 1].imgdir;
+		$scope.imgs				= $scope.projects[$routeParams.projectId - 1].imgs;
+		$scope.dropdowns 	= $scope.projects[$routeParams.projectId - 1].dropdowns;
+		$scope.links 			= $scope.projects[$routeParams.projectId - 1].links;
+		ProjectImages.get($scope.projects[$routeParams.projectId - 1].imgdir, function(response) {
+			$scope.imgs = response;
+		});
+	});
+}])
+
+//---------------------------------------------
+//----- PROJECT THUMBS CONTROLLER -----//
+//---------------------------------------------
+
+.controller('project_thumbs_ctrl', ['Images', 'ProjectImages', '$scope', '$routeParams', function(Images, ProjectImages, $scope, $routeParams) {
+	ProjectImages.get($scope.projects[$routeParams.projectId - 1].imgdir, function(response) {
+		$scope.projectImages = response;
+		$scope.imageLoading = true;
+		var image = 'img/' + $scope.projects[$routeParams.projectId - 1].imgdir + '/' + $scope.projectImages[$scope.$index];
+		Images.preload(image, function() {
+			$scope.stopSpinner();
+			$scope.imageLoading = false;
+			if(!$scope.$$phase) {
+				$scope.$digest();
+			}
+		});
 		$(".fancybox").fancybox({
 			padding: [0,0,0,0]
 		});
-		
-		$("[data-toggle='tooltip']").tooltip();
-		
-		$timeout(function() {
-			
-			//initialise spinner
-			var opts = {
-				lines: 5, // The number of lines to draw
-				length: 0, // The length of each line
-				width: 10, // The line thickness
-				radius: 16, // The radius of the inner circle
-				corners: 0, // Corner roundness (0..1)
-				rotate: 0, // The rotation offset
-				direction: 1, // 1: clockwise, -1: counterclockwise
-				color: '#49adf6', // #rgb or #rrggbb or array of colors
-				speed: 1, // Rounds per second
-				trail: 68, // Afterglow percentage
-				shadow: false, // Whether to render a shadow
-				hwaccel: true, // Whether to use hardware acceleration
-				className: 'spinner', // The CSS class to assign to the spinner
-				zIndex: 2e9, // The z-index (defaults to 2000000000)
-				top: 'auto', // Top position relative to parent in px
-				left: 'auto' // Left position relative to parent in px
-			};
-			var targets = $('.image-placeholder');
-			var spinners = [];
-			for(var i = 0; i < targets.length; i++) {
-				spinners[i] = new Spinner(opts).spin(targets[i]);
-			}
-			
-			function PreloadImage(imgSrc, callback, imgs, spinners, idx){
-				var objImagePreloader = new Image();
-			
-				objImagePreloader.src = imgSrc;
-				if(objImagePreloader.complete){
-					callback(imgs, spinners, idx);
-					objImagePreloader.onload=function(){};
-				}
-				else{
-					objImagePreloader.onload = function() {
-						callback(imgs, spinners, idx);
-						//    clear onLoad, IE behaves irratically with animated gifs otherwise
-						objImagePreloader.onload=function(){};
-					}
-				}
-			}
-			
-			$scope.imgsLoading = [];
-			
-			var imgcallback = function(imgs, spinners, idx) {
-				spinners[idx].stop();
-				imgs[idx] = false;
-				$scope.$apply();
-				//console.log('finished loading... ' + idx);
-				//console.log(imgs);
-			};
-			
-			for(var i = 0; i < $rootScope.projects[$routeParams.projectId - 1].imgs.length; i++) {
-				var img = 'img/' + $rootScope.projects[$routeParams.projectId - 1].imgdir + '/' + 	$rootScope.projects[$routeParams.projectId - 1].imgs[i];
-				//console.log('started loading... ' + i);
-				$scope.imgsLoading[i] = true;
-				//console.log($scope.imgsLoading);
-				PreloadImage(img, imgcallback, $scope.imgsLoading, spinners, i);
-			}
-			
-		}, 0);
 	});
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,183 +6,52 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-	.service('Projects', function($rootScope, $http, $timeout) {
-		
-		//projects
-		$rootScope.projects = [
-			{ 
-				title: 	'Brothers Tale', 
-				at: 		'VFS',
-				what:		['Idea', 'UX/UI', 'Art', 'GameDesign', 'LevelDesign', 'Modelling', 'Texturing', 'WeightPainting', 'Animating', 'C#'],
-				info: 	"Ages ago, Death wanted to retire. However, in order to do that, he had to kill every living creature. In an ancient race of people, four brothers became heroes who fought Death. They harnessed the power of the elements — earth, air, fire and water — by each wearing a magical mask imbued with these powers. They fought valiantly and, ultimately, denied Death in his bid to destroy mankind. Yet they sacrificed their lives in the process to save the people they loved. And they left behind a fifth brother — a little boy — who had been too young to fight. Now, he is a man embarking on a quest to kill Death and avenge the demise of his brothers.",
-				imgdir:	'brothers-tale',
-				dropdowns: [
-					{
-						title: 'look at case studies',
-						links: [
-							{
-								title: 'read the blog',
-								url: 'http://community.vfs.com/oomph/2011/10/game-design-yeah-digital-design-students-can-do-that/',
-								target: '_blank'
-							},
-							{
-								title: 'watch the video',
-								url: 'http://vimeo.com/32888874',
-								target: '_blank'
-							}
-						]
-					},
-					{
-						title: 'dowload the game',
-						links: [
-							{
-								title: 'for Windows',
-								url: '#'
-							},
-							{
-								title: 'for Mac',
-								url: '#'
-							}
-						]
-					}
-				]
-			},
-			{
-				title: 	'Shogunee', 
-				at: 		'Just for fun',
-				what:		['Idea', 'UX/UI', 'Art', 'GameDesign', 'AS3'],
-				info: 	"I have a certain affinity for the games I played growing up. I loved Tower Defense games, so when my abilities in Flash developed, I was able to create my own using AS3. The point of the game is to protect your castle from ninjas and samurais hordes by placing different towers in the best possible spot. Players need to be careful, because there are invisible ninjas and other enemies who can attack and take out the towers. Players can sell, repair and upgrade their towers for better protection. Shogunee was an experiment in Flash development. My goal was to take on the challenge of creating a fully functional Flash game from scratch. The project was successful and was instrumental in developing my abilities with Action Script, Flash and Adobe Illustrator.",
-				imgdir:	'shogunee',
-				links: [
-					{
-						title: 'play Shogunee',
-						url: '#'
-					}
-				]
-			},
-			{
-				title: 	'Zootaire', 
-				at: 		'Just for fun',
-				what:		['Idea', 'UX/UI', 'AS3'],
-				info: 	"I got an assignment in University to create a soliter table game —not solitaire, the card game!— using Visual Studio. I loved the project so much that I decided to turn it into a Flash game. I turned my C# code into AS3 and created a cool, childish design for it. I wanted to make the game for kids to enhance their logic capacities. The goal of Zootaire is to reduce the number of stones —or in this case sheeps— all the way down to one by moving stones to the side of a neighboring stone, but only if there is an empty space. Movements can be made vertically or horizontally, not diagonally.",
-				imgdir:	'zootaire',
-				links: [
-					{
-						title: 'play Zootaire',
-						url: '#'
-					}
-				]
-			},
-			{
-				title: 	'CBC - Sodium Calculator', 
-				at: 		'Ayogo Inc.',
-				what:		['UX/UI', 'Time/Team/Client Management', 'Supervised Coding', 'HTML5 & CSS3', 'jQuery'],
-				client:	'CBC - Marketplace',
-				info: 	"Suddenly we got an opportunity to work for CBC Marketplace on an online, interactive html5 and css3 based (so no flash) sodium calculator for their upcoming show. The deadline was super close, we only got two weeks before the show started. Becuse of budget and timing, Ayogo assigned this project to me as a one man army, though I got a programmer intern to help me out and start programming the logic parallel while I was designing the UI and UX. In this two week, my responsibilities spanned from talking to the client, keeping my boss in the loop, building a fast and non-redundant work process between me and Allen (my little helper, who helped a LOT), Designing UX/UI from scratch, using stock icons where possible for foods and at the end, ironing out bugs in the code. Oh, almost forgot to mention, that this 2 week was shortened by 2 days also because I requested some vacation time off before we even knew about this project, and that vacation time was really precious to me, because my girlfriend left the country, so wanted to spend my last days with her only. So even with -2 days, we delivered before time, way under budget and the client was super happy and impressed with the quality of work in this short time we had to deliver.",
-				imgdir:	'cbc-sodium',
-				links: [
-					{
-						title: 'check it out',
-						url: 'http://www.cbc.ca/news/interactives/sodium-calculator/index.html',
-						target: '_blank'
-					}
-				]
-			},
-			{
-				title: 	'I<3J', 
-				at: 		'Ayogo Inc.',
-				what:		['UX', 'C#', 'Java'],
-				client: 'Avery Dennison - Vancive',
-				info:		"A project that I feel... failed. So why am I showing this? I think we can learn just as much from failures (if not more) than successes. Also this project involved a lot of heat, pressure, frustration and cutting edge technology and was overall fun and really interesting. Also was my first mobile unity project. And if that's not enough, throw in a cutting edge hardware and software (it came in parts so we had to assemble it ourselves), a non-exsiting API and Bluetooth. All served cold. :D hahaha. So Avery Dennison hired Ayogo to create an Android mobile game that uses their prototype patch to measure all kinds of activity data like heart rate, respiration rate, body temperature, sweat level, movement (gyro, or steps) and so on. This little patch saves data internally. In certain periods it tryes to connect to a mobile device and sends it's saved data throught Bluetooth for further processing. The client wanted a game that reacts to this 'real time' data feed in real time and you know, just be fun, casual and make kids (oh right, we targeted kids) get up and do a little exercise now and then. The game is meant to be showcased with the patch, and to see what and how the patch can be used and integrated. We did real life tests of just sitting and being calm, walking, running, doing jumpingjacks, doing a full workout and such to realise that the patch was unreliable in measuring bio data and in connectivity. The showcases still wen't smoothly though, but just wasn't using live data from the patches. Also we wen't way over budget (around 300%) and time too.",
-				imgdir:	'zootaire'
-			},
-			{
-				title:	'Ayogo Website Update', 
-				at:			'Ayogo Inc.',
-				what:		['UI', 'Supervised Coding', 'HTML5 & CSS3', 'jQuery', 'AngularJS'],
-				info:		'Something here...',
-				imgdir:	'ayogo-website-update',
-				links: [
-					{
-						title: 'check it out',
-						url: 'http://ayogo.com/',
-						target: '_blank'
-					}
-				]
-			}
-		];
-		
-		//return projects only after we got the images
-		var run = false;
-		return {
-			
-			getProjects : function(callback) {
-				
-				if(run && !!callback) {
-					$timeout(function() {
-						$rootScope.scrollr.refresh();
-					}, 0);
-					callback($rootScope.projects);
-					return;
-				}
-				
-				var counter = 0;
-				var num_projects = $rootScope.projects.length;
-				
-				// count how many projects we have received
-				var cb = function() {
-					if (++counter == num_projects) {
-						run = true;
-						if (!!callback) {
-							$rootScope.scrollr = skrollr.init({
-								forceHeight: false
-							});
-							callback($rootScope.projects);
-						}
-					}
-				}
-				
-				// get project images
-				for (var i = 0; i < num_projects; i++) {
-					(function(proj_idx) {
-						if (typeof($rootScope.projects[proj_idx].imgdir) != 'undefined') {
-							$http.get('php/getProjectImages.php?imgdir=' + $rootScope.projects[proj_idx].imgdir)
-							.success(function(data) {
-								$rootScope.projects[proj_idx].imgs = data;
-								cb();
-							})
-							.error(function(data, status, header, config) {
-								//console.error(data);
-							});
-						}
-					})(i);
-				}
-					
-			}
-		};
-		
-	})
-	
-	.service('Images', function() {
-		
-		return {
-			preload : function (imgSrc, callback){
-				var objImagePreloader = new Image();
-			
-				objImagePreloader.src = imgSrc;
-				if(objImagePreloader.complete){
+
+//---------------------------------------------
+//----- PROJECTS SERVICE -----//
+//---------------------------------------------
+
+.service('Projects', function($http) {
+	this.get = function(callback) {
+		$http.get('projects.json').then(function(response) {
+			callback(response.data);
+		});
+	}
+})
+
+//---------------------------------------------
+//----- PROJECT IMAGES SERVICE -----//
+//---------------------------------------------
+
+.service('ProjectImages', function($http) {
+	this.get = function(imgdir, callback) {
+		$http.get('php/getProjectImages.php?imgdir=' + imgdir).then(function(response) {
+			callback(response.data);
+		});
+	}
+})
+
+//---------------------------------------------
+//----- IMAGES SERVICE -----//
+//---------------------------------------------
+
+.service('Images', function() {
+	return {
+		preload : function (imgSrc, callback) {
+			var objImagePreloader = new Image();
+			objImagePreloader.src = imgSrc;
+			if(objImagePreloader.complete) {
+				callback();
+				objImagePreloader.onload = function() {};
+			} else {
+				objImagePreloader.onload = function() {
 					callback();
-					objImagePreloader.onload=function(){};
-				}
-				else{
-					objImagePreloader.onload = function() {
-						callback();
-						//clear onLoad, IE behaves irratically with animated gifs otherwise
-						objImagePreloader.onload=function(){};
-					}
+					//clear onLoad, IE behaves irratically with animated gifs otherwise
+					objImagePreloader.onload = function() {};
 				}
 			}
-				
 		}
-		
-	});
+			
+	}
+	
+});

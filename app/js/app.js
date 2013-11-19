6c46ded557150f54636962150f3d94705c2a26c0
levick <levente.opelcz@gmail.com>
2013-11-18 23:16:19 -0800

    - ng route fix coz of new angular - ng-href instead of href fixes

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'home_ctrl'});
     $routeProvider.when('/project/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});

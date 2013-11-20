0fc2dcda01318981ad55beee581062266e86a61e
levick <levente.opelcz@gmail.com>
2013-11-20 00:53:22 -0800

    Navigating site should be as per normal websites. New page should jump to the top of the page. Dunno why is this so hard and why this isn't the default behaviour...

--- app/js/app.js
+++ app/js/app.js
@@ -7,4 +7,10 @@ angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.di
 		$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'home_ctrl'});
     $routeProvider.when('/project/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});
     $routeProvider.otherwise({redirectTo: '/'});
-  }]);
+  }])
+	.run(function($rootScope, $anchorScroll) {
+		$rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+			//scroll to top of page as per every other god damn page on the web :/
+			$anchorScroll();  
+		});
+	});

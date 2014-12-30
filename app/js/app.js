42443227e873dd2f40201364305a64bb07aa9473
Levente Opelcz <levente.opelcz@gmail.com>
2014-12-30 17:58:14 +0100

    Cleanup, adding new group to projects (wip and/or canned).

--- app/js/app.js
+++ app/js/app.js
@@ -7,8 +7,10 @@ angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.di
   $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'home_ctrl'});
   $routeProvider.when('/project/pro/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});
   $routeProvider.when('/project/hs/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});
+  $routeProvider.when('/project/wip/:projectId', {templateUrl: 'partials/project.html', controller: 'project_ctrl'});
   $routeProvider.when('/shogunee', {templateUrl: 'partials/shogunee.html'});
   $routeProvider.when('/zootaire', {templateUrl: 'partials/zootaire.html'});
+  $routeProvider.when('/match3', {templateUrl: 'partials/match3.html'})
   $routeProvider.otherwise({redirectTo: '/'});
 }])
 .run(function($rootScope, $anchorScroll) {
